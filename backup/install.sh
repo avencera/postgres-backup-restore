fe52d82a7dd2912defc7d27217c6c7eb7949a38e
Praveen Perera <praveenperera@users.noreply.github.com>
2019-04-24 14:00:31 -0400

    Use alpine virtual deps

--- backup/install.sh
+++ backup/install.sh
@@ -9,16 +9,16 @@ apk update
 apk add --no-cache postgresql-client
 
 # install s3 tools
-apk add python py2-pip
+apk add --no-cache python py2-pip
 pip install awscli
-apk del py2-pip
+apk del --no-cache py2-pip
 
 # install go-cron
-apk add curl
+apk add curl --no-cache --virtual .build-deps
 curl -L --insecure https://github.com/odise/go-cron/releases/download/v0.0.6/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
 chmod u+x /usr/local/bin/go-cron
 apk del curl
-
+apk del .build-deps 
 
 # cleanup
 rm -rf /var/cache/apk/*
